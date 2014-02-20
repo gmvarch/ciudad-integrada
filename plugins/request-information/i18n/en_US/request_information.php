a0045ead97d7523b44b33d1e1a46834de330d604
Jose <jose@dev.wingu2.com>
2014-02-20 15:16:58 -0300

    request information mal

--- plugins/request-information/i18n/en_US/request_information.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$lang = array(
-		'category'=>'Please select at least one category to download',
-		'from_date'=>'Please enter a valid FROM Date',
-		'to_date'=>'Please enter a valid TO Date',
-		'required'=>'This field is required',
-	);
-?>
